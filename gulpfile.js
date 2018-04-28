01eb54e46eaec160a0855d2ad32c50ec4757a106
DESKTOP-CTSU47C\Asus <l3o.f3rr3ira@gmail.com>
2018-04-28 12:25:25 +0100

    atualizacao gitignore + add do package, gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp'),
+      sass = require('gulp-sass');
+
+gulp.task("sass", function () {
+    gulp.src("./src/scss/**/*.scss")
+    .pipe(sass())
+    .pipe( gulp.dest("./dist"));
+})
+
+gulp.task("watch", function() {
+    gulp.watch("./src/scss/**/*.scss", ["sass"])
+})
