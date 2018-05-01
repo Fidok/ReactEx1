725e36e2b56931a0a8a340323fc3f3c59a79a593
DESKTOP-CTSU47C\Asus <l3o.f3rr3ira@gmail.com>
2018-05-01 18:38:43 +0100

    Correction command cp

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,16 @@ gulp.task("sass", function () {
     .pipe( gulp.dest("./dist"));
 })
 
+gulp.task("build", function () {
+    gulp.src("node_modules/bootstrap/dist/css/*.css")
+    .pipe( gulp.dest("./dist/components/bootstrap"));
+        gulp.src("node_modules/font-awesome/**/*.*")
+    .pipe( gulp.dest("./dist/components/font-awesome"));        gulp.src("src/index.html")
+    .pipe( gulp.dest("./dist"));
+})
+
 gulp.task("watch", function() {
     gulp.watch("./src/scss/**/*.scss", ["sass"])
 })
 
-gulp.task("default", ["sass"]);
+gulp.task("default", ["build","sass"]);
